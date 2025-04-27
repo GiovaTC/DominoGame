534c0e7d3134d012b5f772415e62bbfca07f78bf
Giovanny <giovanny.tapiero@pi.edu.co>
2025-04-27 18:57:19 -0500

    Agregar archivos de proyecto.

---
+++ DominoGame/DominoGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoGame
+{
+    public class DominoGame
+    {
+        public List<DominoTile> Deck { get; private set; }
+        public List<DominoTile> Table { get; private set; }
+        public List<DominoTile> PLayerHand { get; private set; }
+
+        public DominoGame()
+        {
+            Deck = GenerateDeck();
+            ShuffleDeck();
+     //     PlayerHand = DrawTiles(7);
+            Table = new List<DominoTile>();
+        }
+
+        private List<DominoTile> GenerateDeck()
+        {
+            var deck = new List<DominoTile>();
+            for (int i = 0; i <= 6; i++)
+            {
+                for (int j = i; j <= 6; j++)
+                {
+                    deck.Add(new DominoTile(i, j));
+                }
+            }
+            return deck;
+        }
+
+        private void ShuffleDeck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DominoGame/DominoTile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoGame
+{
+    public class DominoTile
+    {
+        public int Left { get; set; }
+        public int Right { get; set; }
+
+        public DominoTile(int left, int right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public DominoTile Flip()
+        {
+            return new DominoTile(Right, Left);
+        }
+
+        public override string ToString()
+        {
+            return $"{Left} | {Right}";
+        }
+    }
+}
